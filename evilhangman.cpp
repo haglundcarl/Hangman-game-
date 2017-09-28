a67a1566fd284484d13c3e141dfff231220aae1a
haglundcarl <31624471+haglundcarl@users.noreply.github.com>
2017-09-28 12:18:42 +0200

    Add files via upload

--- evilhangman.cpp
+++ evilhangman.cpp
@@ -13,10 +13,16 @@ const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
 int main();
 
+void gameLoop();
+
+void checkNumber(ifstream& file, set<string> dict, vector<string>& wordsLeft, int& wordLength);
+
 void viewRest(bool& vrb){
+
     string viewRestString;
     cout << "Do you want to see how many words you have left after each guess? y/n"<< endl;
     cin >> viewRestString;
+
     if(viewRestString == "y"){
       vrb = true;
     }
@@ -35,7 +41,6 @@ void guessLetter(string& guess){
     cin >> guess;
 
     for(auto a: ALPHABET){
-
         if( guess[0] == a && guess.length() == 1){
            valid = true;
         }
@@ -45,39 +50,51 @@ void guessLetter(string& guess){
         guessLetter(guess);
     }
 }
+
 void playAgain(){
+
     string ans;
     cout << "Do you want to play again?! (y/n)" << endl;
     cin >> ans;
-    if(ans == "y"){
-        main();
+    if(cin.fail()){
+        cin.clear();
+        cin.ignore(256, '\n');
+        playAgain();
     }
-    else if(ans == "n"){
-        exit(0);
+    else{
+        if(ans == "y"){
+            main();
+        }
+        else if(ans == "n"){
+            exit(0);
+        }
+        else{
+            playAgain();
+        }
     }
 }
 
 void endGame(string str, vector<string> myVector, int guessNumb){
+
     if(str == myVector.at(0)){
-        cout << "You WON!!" << endl;
+        cout << "You WON!! Your word was " << str << endl;
         playAgain();
     }
-    if(guessNumb == 0){
-        cout<<"You lose. The word you were looking for was: "<< myVector.at(0) <<endl;
+    if(guessNumb <= 0){
+        cout << "You lose. The word you were looking for was: " << myVector.at(0) <<endl;
         playAgain();
     }
 }
 
-void divideFamilies(vector<string> myVector, string guess, string& newstr, int guessNumb, bool viewWords){// returns the largest set
-
-    map<string,vector<string>> myMap;
+void divideFamily(map<string,vector<string>>& myMap,vector<string> myVector, string guess, string& newstr){
     string str;
 
     for(vector<string>::iterator i = myVector.begin(); i != myVector.end(); i++){
         string word = *i;
         str = "";
 
-        for(int j = 0; j < word.length(); ++j){
+        int wordLength = word.length();
+        for(int j = 0; j < wordLength; ++j){
 
                 if(word[j] == guess[0]){
                     str.append(guess);
@@ -91,13 +108,13 @@ void divideFamilies(vector<string> myVector, string guess, string& newstr, int g
         myMap[str].push_back(word);
     }
 
+}
 
-    int currentSize = 0;
-    vector<string> v1;
+void sizeOfVector(int& currentSize, map<string,vector<string>>& myMap, string& newstr, vector<string>& v1){
 
     for(pair<string, vector<string>> newVector : myMap){
 
-        int words = newVector.second.size();        
+        int words = newVector.second.size();
 
         if(words > currentSize){
             newstr = newVector.first;
@@ -105,6 +122,18 @@ void divideFamilies(vector<string> myVector, string guess, string& newstr, int g
             currentSize = words;
         }
     }
+}
+
+void gameLoop(vector<string> myVector, string guess, string& newstr, int guessNumb, bool viewWords){// returns the largest set
+
+    map<string,vector<string>> myMap;
+    vector<string> v1;
+    int currentSize = 0;
+
+    divideFamily(myMap, myVector, guess, newstr);
+
+    sizeOfVector(currentSize, myMap, newstr, v1);
+
     guessNumb -= 1;
     endGame(newstr, v1, guessNumb);
 
@@ -117,35 +146,68 @@ void divideFamilies(vector<string> myVector, string guess, string& newstr, int g
 
     guessLetter(guess);
 
-    divideFamilies(v1,guess, newstr, guessNumb, viewWords);
-
+    gameLoop(v1,guess, newstr, guessNumb, viewWords);
 }
 
-int guessAmount(){
-    int guess;
+void guessAmount(int& guessNumb){
+
+    int currGuess;
     cout << "Please write an amount > 0 of guesses you want" << endl;
-    cin >> guess;
+    cin >> currGuess;
+
     if(cin.fail()){
        cin.clear();
        cin.ignore(256, '\n');
-       guessAmount();
+       guessAmount(guessNumb);
     }
+    else if(currGuess < 1){
+        cin.clear();
+        cin.ignore(256, '\n');
+        guessAmount(guessNumb);
+    }
+
     else{
-        return guess;
+         guessNumb = currGuess;
     }
 }
 
-int wrongNumber(){
+void readFile(ifstream& file, set<string> dict, vector<string>& wordsLeft, int wordLength){
+    string line;
+    while(getline(file, line)){
+        dict.insert(line);
+        int lineLength = line.length();
+        if(lineLength == wordLength){
+            wordsLeft.push_back(line);
+        }
+    }
+
+}
+
+void checkNumber(ifstream& file, set<string> dict, vector<string>& wordsLeft, int& wordLength){
+
     int num;
     cout << "Please write the number of char you want to play with:"<<  endl;
     cin >> num;
-    if(!cin){
-       cin.clear();
-       cin.ignore(256,'\n');
-       wrongNumber();
+
+    if(cin.fail()){
+        cout << "Please write a valid number" << endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        checkNumber(file, dict, wordsLeft, wordLength);
     }
     else{
-        return num;
+        readFile(file, dict, wordsLeft, num);
+        if(wordsLeft.empty()){
+           cout << "Ain't no words with that length" << endl;
+           cin.clear();
+           cin.ignore(256,'\n');
+           file.close();
+           file.open("dictionary.txt");
+           checkNumber(file, dict, wordsLeft, wordLength);
+       }
+       else{
+            wordLength = num;
+       }
     }
 }
 
@@ -154,28 +216,16 @@ int main() {
     int guessNumb;
     bool viewRestBool;
     string guess;
-    cout << "Welcome to Hangman." << endl;
-
-    wordLength = wrongNumber();
 
     ifstream file ("dictionary.txt");
-    string line;
     set<string> dictionary;
     vector<string> wordsLeft;
 
-    while(getline(file, line)){
-        dictionary.insert(line);
-        if(line.length() == wordLength){
-            wordsLeft.push_back(line);
-        }
+    cout << "Welcome to Hangman." << endl;
 
-    }
-    if(wordsLeft.empty()){
-        cout << "Ain't no words with that length" << endl;
-        exit(0);
-    }
+    checkNumber(file, dictionary, wordsLeft, wordLength);
 
-    guessNumb = guessAmount();
+    guessAmount(guessNumb);
 
     viewRest(viewRestBool);
 
@@ -184,20 +234,15 @@ int main() {
     for(int i = 0; i < wordLength; i++){
         cout << "-" ;
     }
-                cout << endl;
 
-    /*if(viewRestBool == true){
-        cout << "words left: " << endl;
-        for(auto w : wordsLeft){
-            cout<< w << endl;
-        }
-    }*/
     cout << endl;
-    string s = "";
+    cout << endl;
+
+    string startKey = "";
 
     guessLetter(guess);
-    divideFamilies(wordsLeft, guess, s, guessNumb, viewRestBool);
-    // TODO: Finish the program!
+
+    gameLoop(wordsLeft, guess, startKey, guessNumb, viewRestBool);
 
     return 0;
 }
